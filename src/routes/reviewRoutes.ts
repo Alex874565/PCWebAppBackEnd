f99b9ce5ff8e19af89da51d2ed493cff128e4151
Alex874565 <alex.carpinisan@gmail.com>
2024-05-17 01:41:03 +0300

    Changed authorizations

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -3,9 +3,9 @@ const router = express.Router();
 const reviewController = require('../controllers/reviewController');
 const authController = require("../controllers/authController");
 
-router.get('/', authController.authAdminPrivileges, reviewController.getReviews)
-router.get('/product/:product_id', authController.authClientPrivileges, reviewController.getProductReviews)
-router.post('/', authController.authAdminPrivileges, reviewController.createReview)
+router.get('/', reviewController.getReviews)
+router.get('/product/:product_id', reviewController.getProductReviews)
+router.post('/', authController.authClientPrivileges, reviewController.createReview)
 router.put('/:id', authController.authClientPrivileges, reviewController.updateReview)
 router.delete('/:id', authController.authClientPrivileges, reviewController.deleteReview)
 
